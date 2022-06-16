587989de21c643e0f41713cab49ec2b0e405f2c8
DraskoCODE <Mj2612Mj2401>
2022-06-16 16:29:32 +0200

    Add VerifyLoginWithValidCredentials test

--- src/main/java/com/code/company.java
+++ src/main/java/com/code/company.java
@@ -4,25 +4,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class company {
 
-    public static void main(String[] args) {
+    @Test
+    public void Test1() {
+    //public static void main(String[] args) {
         System.out.println("xa");
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\comp\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         //System.out.println("Test Gitt");
-        driver.get("https://code.edu.rs/");
+        driver.get("https://www.saucedemo.com/");
 
         driver.manage().window().maximize();
 
-        WebElement lnkObuke = driver.findElement(By.xpath(".//a[@href='/obuke/']"));
-        lnkObuke.click();
+        WebElement inpUserName = driver.findElement(By.id("user-name"));
+        inpUserName.click();
+        inpUserName.sendKeys("standard_user");
 
-        WebElement lnkJavaDevelopment = driver.findElement(By.xpath(".//a[@href='/obuke/java-development/']"));
-        lnkJavaDevelopment.click();
+        WebElement inpPassword = driver.findElement(By.id("password"));
+        inpPassword.click();
+        inpPassword.sendKeys("secret_sauce");
 
-        driver.close();
+        WebElement btnLogin = driver.findElement(By.id("login-button"));
+        btnLogin.click();
+
+
+        Assert.assertEquals(true, driver.findElement(By.xpath("//*[@id='inventory_container']")).isDisplayed());
+
+        driver.quit();
 
     }
 
